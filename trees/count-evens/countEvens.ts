bf34a056a0519abbffae7e3b40d52605525efc7f
Charles Chitty <charlescchitty@gmail.com>
2024-02-28 12:08:26 -0800

    finished sum-values

--- trees/count-evens/countEvens.ts
+++ trees/count-evens/countEvens.ts
@@ -4,7 +4,30 @@ import { TreeNodeNum } from "../common/tree";
  * children, count how many nodes have even values. Returns that count as
  * an integer. */
 function countEvens(node: TreeNodeNum): number {
-  return 42;
+  let sum = 0;
+
+  if (node.val % 2 === 0){
+    sum += 1
+  }
+
+  for (const child of node.children) { // [2,3,4]
+    if (child.val % 2 === 0){
+      sum += 1
+    }
+    countEvens(child)
+  }
+
+  return sum
 }
 
 export { countEvens };
+
+//            1
+//          / | \
+//        2   3   4
+//               / \
+//              5   6
+//                   \
+//                    7
+//                     \
+//                      8
